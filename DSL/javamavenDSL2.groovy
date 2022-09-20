76616dfdecc73b7b82d78138a36158e038f8adc2
Zayinochoa <107062963+Zayinochoa@users.noreply.github.com>
2022-09-19 20:32:37 -0400

    Update javamavenDSL2.groovy

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+       git('https://github.com/Zayinochoa/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('Zayinochoa')
+            node / gitConfigEmail('bensaj2212@gmail.com')
         }
     }
     steps {
@@ -17,7 +17,7 @@ job('Java Maven App DSL 2') {
         }
         shell('''
           echo "Entrega: Desplegando la aplicación" 
-          java -jar "/var/jenkins_home/workspace/Java Maven App DSL 2/target/my-app-1.0-SNAPSHOT.jar"
+          java -jar "/var/jenkins_home/workspace/Java_App_con_Maven/target/my-app-1.0-SNAPSHOT.jar"
         ''')  
     }
     publishers {
