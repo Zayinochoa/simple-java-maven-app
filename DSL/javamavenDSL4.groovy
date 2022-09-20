96aea9f0c24be92ff143a5218270a344635db074
Zayinochoa <107062963+Zayinochoa@users.noreply.github.com>
2022-09-20 00:00:53 -0400

    Update javamavenDSL4.groovy

--- DSL/javamavenDSL4.groovy
+++ DSL/javamavenDSL4.groovy
@@ -17,7 +17,7 @@ job('Java Maven App DSL4') {
         }
         shell('''
           echo "Entrega: Desplegando la aplicación"
-		  java -jar /var/jenkins_home/workspace/Java_App_con_Maven/target/my-app-1.0-SNAPSHOT.jar		  
+		  java -jar /var/jenkins_home/workspace/Java_App_con_Maven_4/target/my-app-1.0-SNAPSHOT.jar		  
         ''')  
     }
     publishers {
